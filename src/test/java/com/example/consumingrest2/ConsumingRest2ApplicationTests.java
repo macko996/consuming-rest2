01cd0f1da7c374b567edce4281ab60062340a720
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2020-05-21 09:33:32 +0200

    Initial commit: pulling json from api

---
+++ src/test/java/com/example/consumingrest2/ConsumingRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingrest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
